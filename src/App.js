06c231be33c07eae3cbe76bf99ae530fbbe94f78
Roc Wang <zi.wang@ibm.com>
2020-03-17 13:30:41 -0300

    Update Alert function.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar'
 import Users from './components/users/Users'
 import Search from './components/users/Search'
+import Alert from './components/layout/Alert'
 import axios from 'axios';
 import './App.css';
 
@@ -9,7 +10,8 @@ class App extends Component {
 
   state = {
     users: [],
-    loading: false
+    loading: false,
+    alert: null
   }
 
   //Search Github users
@@ -18,18 +20,27 @@ class App extends Component {
     const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&?client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
     this.setState({ users: res.data.items, loading: false })
   }
+
+  // Clear users from state
   clearUsers = () =>
     this.setState({ users: [], loading: false })
+  // Set Alert
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg, type } })
+    setTimeout(() => this.setState({ alert: null }), 5000)
+  }
 
   render() {
-    const { users, loading } = this.state
+    const { users, loading, alert } = this.state
     return (
       <div className="App">
         <Navbar />
         <div className="container">
+          <Alert alert={alert} />
           <Search searchUsers={this.searchUsers}
             clearUsers={this.clearUsers}
             showClear={users.length > 0 ? true : false}
+            setAlert={this.setAlert}
           />
           <Users loading={loading} users={users} /></div>
 
