592ad56dd09a186eec751a4e1ffa86eccc0260dd
Roc Wang <zi.wang@ibm.com>
2020-03-17 15:26:23 -0300

    Add Single User page and component. Using Router in the code.

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
 
-const UserItem = ({user: {login, avatar_url, html_url}}) => {
+const UserItem = ({ user: { name, company, avatar_url, location, bio, blog, login, html_url, followers, following, public_repos, public_gists, hireable } }) => {
     return (
         <div className="card text-center">
             <img src={avatar_url} alt="" className="round-img" style={{ width: '60px' }}></img>
             <h3>{login}</h3>
             <div>
-                <a href={html_url} className="btn btn-black btn-sm my-1">More</a>
+                <Link to={`/user/${login}`} className="btn btn-black btn-sm my-1">More</Link>
             </div>
         </div>
     )
